8acb67e07f9786000f49996575341be300161b7c
Pato <pevargas2730@gmail.com>
2020-02-17 13:36:19 -0500

    template api test

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,12 @@
-// uncomment line below to register offline cache service worker 
+// uncomment line below to register offline cache service worker
 // navigator.serviceWorker.register('../serviceworker.js');
+//import the client module
 
-if (typeof fin !== 'undefined') {
+if (typeof fin !== "undefined") {
     init();
 } else {
-    document.querySelector('#of-version').innerText =
-        'The fin API is not available - you are probably running in a browser.';
+    document.querySelector("#of-version").innerText =
+        "The fin API is not available - you are probably running in a browser.";
 }
 
 //once the DOM has loaded and the OpenFin API is ready
@@ -14,21 +15,47 @@ async function init() {
     const app = await fin.Application.getCurrent();
     const win = await fin.Window.getCurrent();
 
-    const ofVersion = document.querySelector('#of-version');
+    const ofVersion = document.querySelector("#of-version");
     ofVersion.innerText = await fin.System.getVersion();
 
     //Only launch new windows from the main window.
-    if (win.identity.name === app.identity.uuid) {
-        //subscribing to the run-requested events will allow us to react to secondary launches, clicking on the icon once the Application is running for example.
-        //for this app we will  launch a child window the first the user clicks on the desktop.
-        app.once('run-requested', async () => {
-            await fin.Window.create({
-                name: 'childWindow',
-                url: location.href,
-                defaultWidth: 320,
-                defaultHeight: 320,
-                autoShow: true
-            });
-        });
-    }
+    // if (win.identity.name === app.identity.uuid) {
+    //     subscribing to the run-requested events will allow us to react to secondary launches, clicking on the icon once the Application is running for example.
+    //     for this app we will  launch a child window the first the user clicks on the desktop.
+    //     app.once("run-requested", async () => {
+    //         await fin.Window.create({
+    //             name: "test1",
+    //             url: location.href,
+    //             defaultWidth: 320,
+    //             defaultHeight: 320,
+    //             autoShow: true
+    //         });
+    //     });
+    // }
+}
+
+/* Define a function that will launch a window */
+async function launchWindow(url) {
+    /* Use the await keyword. The function will wait for the 
+        window object to instantiate before resuming execution */
+    const win = await fin.Window.create({
+        /* Provide initialization options to the new window */
+        name: "test2",
+        url: url,
+        defaultWidth: 600,
+        defaultHeight: 400,
+        resizable: false,
+        autoShow: true
+    });
+}
+
+async function unDock() {
+    const window = await fin.Window.getCurrent();
+
+    // await layouts.snapAndDock.undockWindow();
+
+    await layouts.snapAndDock.undockWindow({
+        uuid: window.identity.uuid,
+        name: window.identity.name
+    });
 }
